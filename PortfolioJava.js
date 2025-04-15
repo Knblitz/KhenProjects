dda23f7fa451d2717ac56a45e1676a9dbfd002bf
Knblitz <khenthongwk@gmail.com>
2025-04-16 00:53:15 +0800

    Update PortfolioJava.js v16

--- PortfolioJava.js
+++ PortfolioJava.js
@@ -1,10 +1,10 @@
 document.getElementById('toggle-skills-btn').addEventListener('click', function () {
-    const hiddenSkills = document.querySelectorAll('.hidden-skill');
-    const isHidden = hiddenSkills[0].style.display === 'none' || hiddenSkills[0].style.display === '';
-    
-    hiddenSkills.forEach(skill => {
-      skill.style.display = isHidden ? 'list-item' : 'none';
-    });
-  
-    this.innerHTML = isHidden ? 'View Less <' : 'View More >';
+  const hiddenSkills = document.querySelectorAll('.hidden-skill');
+  const isHidden = hiddenSkills[0].style.display === 'none' || hiddenSkills[0].style.display === '';
+
+  hiddenSkills.forEach(skill => {
+    skill.style.display = isHidden ? 'inline-block' : 'none';
   });
+
+  this.textContent = isHidden ? 'View Less <' : 'View More >';
+});
